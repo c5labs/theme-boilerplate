074f2a5dee549f68acc1bb860aad1e6b8db283ca
Oliver Green <green2go@gmail.com>
2014-11-29 10:32:04 +0000

    Added forbidden and not found page templates.

---
+++ themes/boilerplate_theme/page_forbidden.php
@@ -0,0 +1,15 @@
+<?php 
+defined('C5_EXECUTE') or die("Access Denied.");
+
+/**
+ * Page Forbidden
+ * 
+ * @author Oliver Green <green2go@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ */
+
+$this->inc('elements/header.php'); ?>
+
+<h1>Customise your 401 error</h1>
+
+<?php $this->inc('elements/footer.php');
