88fc14f208829999fb75154af29ed6d9ef5bfce1
Oliver Green <green2go@gmail.com>
2014-11-29 15:13:28 +0000

    Added page theme override examples & add styles.xml information links.

--- themes/boilerplate_theme/page_theme.php
+++ themes/boilerplate_theme/page_theme.php
@@ -4,90 +4,77 @@ namespace Concrete\Package\ConcreteThemePackage\Theme\BoilerplateTheme;
 use Concrete\Core\Page\Theme\Theme as Theme;
 
 class PageTheme extends Theme {
-/*
-    public function registerAssets() {
-        //$this->providesAsset('javascript', 'bootstrap/*');
-        $this->providesAsset('css', 'bootstrap/*');
-        $this->providesAsset('css', 'blocks/form');
-        $this->providesAsset('css', 'blocks/social_links');
-        $this->providesAsset('css', 'blocks/share_this_page');
-        $this->providesAsset('css', 'blocks/feature');
-        $this->providesAsset('css', 'blocks/testimonial');
-        $this->providesAsset('css', 'blocks/date_navigation');
-        $this->providesAsset('css', 'blocks/topic_list');
-        $this->providesAsset('css', 'blocks/faq');
-        $this->providesAsset('css', 'blocks/tags');
-        $this->providesAsset('css', 'core/frontend/*');
-        $this->providesAsset('css', 'blocks/feature/templates/hover_description');
-        $this->requireAsset('css', 'font-awesome');
-        $this->requireAsset('javascript', 'jquery');
-        $this->requireAsset('javascript', 'picturefill');
-    }
 
-    protected $pThemeGridFrameworkHandle = 'bootstrap3';
-
-    public function getThemeBlockClasses()
-    {
-        return array(
-            'feature' => array('feature-home-page'),
-            'page_list' => array(
-                'recent-blog-entry',
-                'blog-entry-list',
-                'page-list-with-buttons',
-                'block-sidebar-wrapped'
-            ),
-            'next_previous' => array('block-sidebar-wrapped'),
-            'share_this_page' => array('block-sidebar-wrapped'),
-            'content' => array(
-                'block-sidebar-wrapped',
-                'block-sidebar-padded'
-            ),
-            'date_navigation' => array('block-sidebar-padded'),
-            'topic_list' => array('block-sidebar-wrapped'),
-            'testimonial' => array('testimonial-bio'),
-            'image' => array(
-                'image-right-tilt',
-                'image-circle'
-            )
-        );
-    }
-
-    public function getThemeAreaClasses()
-    {
-        return array(
-            'Page Footer' => array('area-content-accent')
-        );
-    }
-
-    public function getThemeDefaultBlockTemplates()
-    {
-        /*
-        return array(
-            'image' => 'some_special_image_template'
-        );
-        
-    }*/
-/*
-    public function getThemeResponsiveImageMap()
-    {
-        return array(
-            'large' => '900px',
-            'medium' => '768px',
-            'small' => '0'
-        );
-    }
-
-    public function getThemeEditorClasses()
-    {
-        return array(
-            array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin'),
-            array('title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold'),
-            array('title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps'),
-            array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption'),
-            array('title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default'),
-            array('title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success'),
-            array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary')
-        );
-    }*/
-
+    /*
+     * See http://goo.gl/EbMMXu for complete list of 
+     * methods available within this class.
+     *
+     * Examples are:
+     */
+    
+    /**
+     * Register theme assets, see http://goo.gl/rpJUSy
+     *
+     * public function registerAssets() {
+     *    $this->providesAsset('css', 'bootstrap/*');
+     *    $this->providesAsset('css', 'blocks/form');
+     *    $this->requireAsset('javascript', 'jquery');
+     * }
+     */
+    
+    /** 
+     * Set the grid framework see http://goo.gl/1MFxbL
+     * 
+     * protected $pThemeGridFrameworkHandle = 'bootstrap3';
+     * 
+     */
+     
+    /**
+     * Set the block CSS class overrides 
+     *
+     * public function getThemeBlockClasses()
+     * {
+     *     return array(
+     *         'next_previous' => array('block-sidebar-wrapped'),
+     *         'share_this_page' => array('block-sidebar-wrapped'),
+     *         'content' => array('block-sidebar-wrapped'),
+     *         'date_navigation' => array('block-sidebar-padded')
+     *     );
+     * } 
+     */
+    
+    /**
+     * Set theme area CSS class overides
+     *
+     * public function getThemeAreaClasses()
+     * {
+     *     return array(
+     *         'Page Footer' => array('area-content-accent')
+     *     );
+     * }
+     */
+     
+    /**
+     * Set default blocks template for the theme
+     *
+     * public function getThemeDefaultBlockTemplates()
+     * {
+     *     return array(
+     *         'image' => 'some_special_image_template'
+     *     );
+     * }
+     */
+     
+    /**
+     * Set theme content editor styles classes
+     * 
+     * public function getThemeEditorClasses()
+     * {
+     *     return array(
+     *         array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin'),
+     *         array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption'),
+     *         array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary')
+     *     );
+     * }
+     */
 }
