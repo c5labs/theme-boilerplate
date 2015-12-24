dc7aa06b4a403677b2193f18c318ec9506fa75d5
Oliver Green <green2go@gmail.com>
2015-12-24 12:17:37 +0000

    CS & Documentation.

--- controller.php
+++ controller.php
@@ -1,32 +1,76 @@
 <?php
 namespace Concrete\Package\ConcreteThemePackage;
 
-use PageTheme;
-use Package;
- 
+use Concrete\Theme\Concrete\PageTheme;
+use Concrete\Core\Package\Package;
+
 defined('C5_EXECUTE') or die('Access Denied.');
- 
-class Controller extends Package 
+
+/**
+ * Theme package.
+ */
+class Controller extends Package
 {
+    /**
+     * Package handle.
+     *
+     * @var string
+     */
     protected $pkgHandle = 'concrete_theme_package';
+
+    /**
+     * Minimum required version of concrete5 for this package.
+     *
+     * @var string
+     */
     protected $appVersionRequired = '5.7.1';
+
+    /**
+     * Package version.
+     *
+     * @var string
+     */
     protected $pkgVersion = '2.0.0';
 
-    public function getPackageName() 
+    /**
+     * Get the package name.
+     *
+     * @return string
+     */
+    public function getPackageName()
     {
         return t("Concrete Theme Package");
     }
 
-    public function getPackageDescription() 
+    /**
+     * Get the package description.
+     *
+     * @return string
+     */
+    public function getPackageDescription()
     {
         return t("A package that installs a boilerplate theme for 5.7.x.");
     }
 
+    /**
+     * Install routine.
+     *
+     * @return \Concrete\Core\Package\Package
+     */
     public function install()
     {
         $pkg = parent::install();
 
-        // Install the theme
         $theme = PageTheme::add('boilerplate_theme', $pkg);
+    }
+
+    /**
+     * Uninstall routine.
+     *
+     * @return void
+     */
+    public function uninstall()
+    {
+        parent::uninstall();
     }
 }
